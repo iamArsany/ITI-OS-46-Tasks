d9b740d55606a3a0d5ff12cd78bb2ca06947e242
Arsany Ashraf <arsanyarsany4800@gmail.com>
2026-03-14 07:01:43 +0200

    Refactor PostController to manage posts with an array structure and implement CRUD operations

--- 20.laravel/Day1/blogSystem/app/Http/Controllers/PostController.php
+++ 20.laravel/Day1/blogSystem/app/Http/Controllers/PostController.php
@@ -6,66 +6,59 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
-    {
-        $posts = [
-            [
-                'id' => 1,
-                'title' => 'first post',
-                'description' => 'some description',
-                'created_at' => '2026-03-11 10:00:00',
-                'creator' => [
-                    'name' => 'Ahmed',
-                    'email' => 'ahmed@gmail.com',
-                    'created_at' => '2024-09-01 08:00:00'
-                ]
-            ],
-            [
-                'id' => 2,
-                'title' => 'second post',
-                'description' => 'some description 2',
-                'created_at' => '2026-03-11 10:00:00',
-                'creator' => [
-                    'name' => 'Mohamed',
-                    'email' => 'mohamed@gmail.com',
-                    'created_at' => '2024-09-01 08:00:00'
-                ]
-                ],
-                [
-                    'id' => 3,
-                    'title' => 'third post',
-                    'description' => 'some description 2',
-                    'created_at' => '2026-03-11 10:00:00',
-                    'creator' => [
-                        'name' => 'Ali',
-                        'email' => 'Ali@gmail.com',
-                        'created_at' => '2024-09-01 08:00:00'
-                    ]
-                ]
-        ];
-
-        return view('posts.index', [
-            'posts' => $posts,
-        ]);
-    }
-
-    public function show()
-    {
-        $innerPost = [
+    private $posts = [
+        [
+            'id' => 1,
             'title' => 'first post',
             'description' => 'some description',
             'created_at' => '2026-03-11 10:00:00',
-            'creator' => [
-                'name' => 'Ahmed',
-                'email' => 'ahmed@gmail.com',
-                'created_at' => '2024-09-01 08:00:00'
-            ]
-        ];
+            'creator' => ['id' => 1, 'name' => 'Ahmed', 'email' => 'ahmed@gmail.com']
+        ],
+        [
+            'id' => 2,
+            'title' => 'second post',
+            'description' => 'some description 2',
+            'created_at' => '2026-03-11 10:00:00',
+            'creator' => ['id' => 2, 'name' => 'Mohamed', 'email' => 'mohamed@gmail.com']
+        ],
+        [
+            'id' => 3,
+            'title' => 'third post',
+            'description' => 'some description 2',
+            'created_at' => '2026-03-11 10:00:00',
+            'creator' => ['id' => 3, 'name' => 'Ali', 'email' => 'ali@gmail.com']
+        ]
+    ];
+    private $creators = [
+        1 => ['name' => 'Ahmed', 'email' => 'ahmed@gmail.com'],
+        2 => ['name' => 'Mohamed', 'email' => 'mohamed@gmail.com'],
+        3 => ['name' => 'Ali', 'email' => 'ali@gmail.com']
+    ];
+    /**
+     * Helper to get posts from session or default array
+     */
+    private function getPosts()
+    {
+        if (!session()->has('posts')) {
+            session(['posts' => $this->posts]);
+        }
+        return session('posts');
+    }
 
-
-        return view('posts.show', [
-            'post' => $innerPost
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' => $this->getPosts()
         ]);
+    }
+
+    public function show($id)
+    {
+        $posts = $this->getPosts();
+        $post = collect($posts)->firstWhere('id', $id);
+
+        if (!$post) abort(404);
+        return view('posts.show', ['post' => $post]);
     }
 
     public function create()
@@ -73,8 +66,81 @@ class PostController extends Controller
         return view('posts.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $posts = $this->getPosts();
+
+        $creatorId = $request->creator;
+
+        $creator = [
+            'id' => $creatorId,
+            'name' => $this->creators[$creatorId]['name'],
+            'email' => $this->creators[$creatorId]['email']
+        ];
+
+        $newPost = [
+            'id' => count($posts) > 0 ? max(array_column($posts, 'id')) + 1 : 1,
+            'title' => $request->title,
+            'description' => $request->description,
+            'created_at' => now()->toDateTimeString(),
+            'creator' => $creator
+        ];
+
+        $posts[] = $newPost;
+
+        session(['posts' => $posts]);
+
+        return to_route('posts.index');
+    }
+
+    public function edit($id)
+    {
+        $posts = $this->getPosts();
+        $post = collect($posts)->firstWhere('id', $id);
+
+        if (!$post) abort(404);
+
+        return view('posts.edit', ['post' => $post]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $posts = $this->getPosts();
+
+        foreach ($posts as $key => $post) {
+
+            if ($post['id'] == $id) {
+
+                $posts[$key]['title'] = $request->title;
+                $posts[$key]['description'] = $request->description;
+
+                $creatorId = $request->creator;
+
+                $posts[$key]['creator'] = [
+                    'id' => $creatorId,
+                    'name' => $this->creators[$creatorId]['name'],
+                    'email' => $this->creators[$creatorId]['email']
+                ];
+
+                break;
+            }
+        }
+
+        session(['posts' => $posts]);
+
+        return to_route('posts.index');
+    }
+
+    public function destroy($id)
+    {
+        $posts = $this->getPosts();
+
+        $filteredPosts = array_filter($posts, function ($post) use ($id) {
+            return $post['id'] != $id;
+        });
+
+        session(['posts' => array_values($filteredPosts)]);
+
         return to_route('posts.index');
     }
 }
